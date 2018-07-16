bee173658a41104a5a321eea8b28e7334807a48f
Nick Henry <nshenry03@gmail.com>
2018-07-16 08:53:43 -0600

    Initial commit

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#===============================================================================
+#
+#          FILE:  install.sh
+# 
+#         USAGE:  ./install.sh 
+# 
+#   DESCRIPTION:  Install Ansible
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR:  Nick Henry (NSH), nicholas.henry@appdirect.com
+#       COMPANY:  AppDirect
+#       VERSION:  1.0
+#       CREATED:  07/16/2018 08:50:09 AM MDT
+#      REVISION:  ---
+#===============================================================================
+
+sudo dnf install -y ansible
